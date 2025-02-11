45018877094059f6d98b259c5cdc8e1d72fe4fe0
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-02-10 23:23:17 -0300

    Projeto Spotify
    Adição do projeto do spotify realizado durante a imersão front-end da Alura (sem os React.js modules)

---
+++ Imersão_Alura_Spotify/spotify-no-react/script.js
@@ -0,0 +1,44 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
+    
+    requestApi(searchTerm);
+})
+// BOTÃO
+
+// Seleciona o botão
+const backButton = document.getElementById('back-button');
+
+// Adiciona um evento de clique
+backButton.addEventListener('click', function() {
+    // Redireciona para a página inicial do Spotify ou para a seção desejada
+    window.location.href = 'index.html'; // Altere para a URL desejada
+});
